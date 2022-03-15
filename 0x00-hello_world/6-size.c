13f6e0308e210a7df65db4093cf257b16833c1ce
Perrie94 <lindaperrie1@gmail.com>
2022-03-15 21:34:37 +0300

    6th commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,6 @@ int main(void)
 	long long int i3;
 	char c;
 	float f;
-
 	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(c));
 	printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(i2));
