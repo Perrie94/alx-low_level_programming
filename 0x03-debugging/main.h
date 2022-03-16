1c3a5c7b91ffa033b3e9b06859fda3ea3b14478c
Perrie94 <lindaperrie1@gmail.com>
2022-03-16 17:27:47 +0300

    2nd commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,16 +1,27 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-#endif
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+    int largest;
+
+    if (a > b && b > c)
+    {
+        largest = a;
+    }
+    else if (b > a && a > c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
+
+    return (largest);
+}
