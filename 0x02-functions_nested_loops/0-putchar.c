e052a678393d19e441b69026955e615fc531eee2
Perrie94 <lindaperrie1@gmail.com>
2022-03-16 12:07:55 +0300

    0 commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "programming.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('P');
+	_putchar('r');
+	_putchar('o');
+	_putchar('g');
+	_putchar('r');
+	_putchar('a');
+	_putchar('m');
+	_putchar('m');
+	_putchar('i');
+	_putchar('n');
+	_putchar('g');
+	_putchar('\n');
+	return (0);
+}
